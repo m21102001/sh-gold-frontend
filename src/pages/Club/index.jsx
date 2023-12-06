80e19bb819bd160b81ae577e6f2719976daef3b1
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-07 01:27:48 +0200

    create Club

---
+++ src/pages/Club/index.jsx
@@ -0,0 +1,15 @@
+import { Footer, Navbar } from "@/layout"
+import { GoldNews } from "@/components"
+
+
+const Club = () => {
+    return (
+        <>
+            <Navbar />
+            <GoldNews />
+            <Footer />
+        </>
+    )
+}
+
+export default Club
