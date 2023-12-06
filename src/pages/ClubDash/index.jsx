08ddd5dcc66c07db0857d792cc6b7815bb4bd3c4
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-07 01:28:21 +0200

    create ClubDash
    create ClubDash

---
+++ src/pages/ClubDash/index.jsx
@@ -0,0 +1,15 @@
+import { SidebarDashboard } from "@/layout"
+
+const ClubDash = () => {
+  return (
+    <div className="dashboard d-flex flex-row">
+      <SidebarDashboard />
+      <div className="container text-center">
+        <h1>Club Dashboard</h1>
+      </div>
+
+    </div>
+  )
+}
+
+export default ClubDash
