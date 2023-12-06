09bcfc17b1cfa492d8b23b7058bdd53c47990c16
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-07 01:23:53 +0200

    create many folder and pages and make a lot of routes
    create many folder and pages and make a lot of routes

--- src/Routers/Routers.jsx
+++ src/Routers/Routers.jsx
@@ -1,20 +1,62 @@
 import { Routes, Route, BrowserRouter as Router } from 'react-router-dom';
-import { ErrorPage, GoldNews, GoldPrice, Home, Login, Payment, Shop, SignUp } from '@/pages';
-import { Navbar } from '@/layout';
+import {
+  BooksDash,
+  Club,
+  ClubDash,
+  ContactFormDash,
+  CreateBookDash,
+  CreateGoldDahs,
+  Dashboard,
+  DeatilsContactFormDash,
+  ErrorPage,
+  GoldDash,
+  GoldNews,
+  GoldPrice,
+  Home,
+  Login,
+  Payment,
+  ProfileDash,
+  ResetPassword,
+  ResetPasswordOtp,
+  Shop,
+  SignUp,
+  UpdateBooksDash,
+  UpdateGoldDash
+} from '@/pages';
+import ContactUs from '@/pages/ContactUS';
 
 const Routers = () => {
   return (
     <div className='conatiner'>
       <Router>
-        <Navbar/>
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/payment" element={<Payment />} />
-          <Route path="/shop" element={<Shop />} />
-          <Route path="/sign-up" element={<SignUp />} />
+          <Route path='/' element={<Home />} />
+          <Route path='/club' element={<Club />} />
           <Route path="/goldPrice" element={<GoldPrice />} />
           <Route path="/gold-news/:id" element={<GoldNews />} />
+          <Route path="/contactUs" element={<ContactUs />} />
+          {/* auth */}
+          <Route path="/auth/shop" element={<Shop />} />
+          <Route path="/auth/sign-up" element={<SignUp />} />
+          <Route path="/auth/payment" element={<Payment />} />
+          <Route path="/auth/login" element={<Login />} />
+          {/* <Route path="/auth/verifyEmail" element={<VerifyEmail />} /> */}
+          <Route path="/auth/resetPassword" element={<ResetPassword />} />
+          <Route path="/auth/resetPasswordOtp" element={<ResetPasswordOtp />} />
+          {/* Dashboard */}
+          <Route path="/dash/dashboard" element={<Dashboard />} />
+          <Route path="/dash/gold" element={<GoldDash />} />
+          <Route path="/dash/create-gold-item" element={<CreateGoldDahs />} />
+          <Route path="/dash/update-gold/:id" element={<UpdateGoldDash />} />
+          <Route path="/dash/books" element={<BooksDash />} />
+          <Route path="/dash/create-books" element={<CreateBookDash />} />
+          <Route path="/dash/update-books/:id" element={<UpdateBooksDash />} />
+          <Route path="/dash/club" element={<ClubDash />} />
+          <Route path="/dash/contact-form" element={<ContactFormDash />} />
+          <Route path="/dash/details-contact-form/:id" element={<DeatilsContactFormDash />} />
+
+          <Route path="/dash/profile" element={<ProfileDash />} />
+
           <Route path="*" element={<ErrorPage />} />
         </Routes>
       </Router>
