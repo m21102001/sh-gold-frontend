0675184c0a8e5225beeda49648bfab20f08f1bc8
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-07 01:25:01 +0200

    identify component
    identify component

--- src/layout/index.js
+++ src/layout/index.js
@@ -1,3 +1,5 @@
+export { default as Root } from './Root';
 export { default as Header } from './Header';
 export { default as Footer } from './Footer';
 export { default as Navbar } from './Navbar';
+export { default as SidebarDashboard } from './SidebarDashboard';
