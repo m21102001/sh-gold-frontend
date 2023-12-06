737e077f9c713a9034aaefc08c3401e9aa10a022
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-07 01:14:42 +0200

    add base url in axios file
    add base url in axios file

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-axios.defaults.baseURL = 'https://www.goldapi.io/api/XAU/USD';
+axios.defaults.baseURL = 'https://goldshop.onrender.com/api';
 axios.defaults.withCredentials = true;
 
 export const api_key = 'goldapi-47gnrlpim79s5-io';
