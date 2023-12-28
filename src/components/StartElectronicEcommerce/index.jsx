526a3152412509e95717f37496b481d14cab007a
m21102001 <mohammedtarek21102001@gmail.com>
2023-12-28 16:45:30 +0200

    update

--- src/components/StartElectronicEcommerce/index.jsx
+++ src/components/StartElectronicEcommerce/index.jsx
@@ -1,40 +1,68 @@
+import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom"
+import book from "../../assets/img/kenzbook.png"
+import { FaArrowLeft } from "react-icons/fa";
+import axios from "@/api/axios";
 
 import "./StartElectronicEcommerce.scss"
-import book from "../../assets/img/kenzbook.png"
-import { useNavigate } from "react-router-dom"
 const StartElectronicEcommerce = () => {
   const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const [bookData, setBookData] = useState([])
+
+  let fetchBook = {
+    method: 'get',
+    url: '/books',
+  };
+  useEffect(() => {
+    setLoading(true);
+    axios.request(fetchBook)
+      .then((response) => {
+        setBookData(response.data);
+        setLoading(false);
+        console.log("bookData", response);
+      })
+      .catch((error) => {
+        setLoading(false);
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="StartElectronicEcommerce my-5 ">
       <div>
         <h2 className="text-light text-center fw-semibold my-3 pt-4">افضل الكتب فى الاستثمار وبيع وشراء الذهب</h2>
-        <div className="Container">
-          <div className="row align-items-center">
-            <div className="col-md-8 col-sm-12 px-5">
-              <h2 className="mb-3 text-end text-light">كل ما تحتاجه قبل الاستثمار فى الذهب</h2>
-              <h3 className="text-end text-light fs-4">
-                تمّ إعداد هذا الكتاب لمُساعدة القارئ على فهم المعدن الثمين (الذهب)، واتّخاذ قرار الاستثمار به. حيثُ تمّ التطرُّق لأنواع الذهب، وعياراته المُختلفة، وأوزانه، وأسعاره، والعوامل المُؤثّرة عليه؛ ليستنّى له دراسة وفهم وضع السوق قبل اتّخاذ قرار الشراء أو البيع. كما تمّ التنويه للسبائك الذهبية وأنواعها، لنضمن إلمام القارئ بكلّ ما يتعلّق بهذا المعدن الأصفر.
-              </h3>
+        {!loading && bookData?.document?.map((item, index) => (
+          index == 3 ? (
+            <div key={index} className="Container">
+              <div className="row align-items-center">
+                <div className="col-md-8 col-sm-12 px-5">
+                  <h2 className="mb-3 text-end text-light">{item?.title}</h2>
+                  <h3 className="text-end text-light fs-4">{item?.description}</h3>
+                </div>
+                <div className="col-md-4 col-sm-12">
+                  <img src={`${book}`} alt="Kenz book" srcSet="Kenz book" className="kenzbook" />
+                </div>
+              </div>
             </div>
-            <div className="col-md-4 col-sm-12">
-              <img src={`${book}`} alt="Kenz book" srcSet="Kenz book" className="kenzbook" />
+          ) : ('')
+        ))}
+        {!loading && bookData?.document?.map((item, index) => (
+          index == 4 ? (
+            <div key={index} className="Container">
+              <div className="row align-items-center">
+                <div className="col-md-4 col-sm-12">
+                  <img src={book} alt="Kenz book" srcSet="Kenz book" className="kenzbook" />
+                </div>
+                <div className="col-md-8 col-sm-12 px-5">
+                  <h2 className="mb-3 text-end text-light">{item?.title}</h2>
+                  <h3 className="text-end text-light fs-4"> {item?.description}</h3>
+                </div>
+              </div>
             </div>
-          </div>
-        </div>
-        <div className="Container">
-          <div className="row align-items-center">
-            <div className="col-md-4 col-sm-12">
-              <img src={book} alt="Kenz book" srcSet="Kenz book" className="kenzbook" />
-            </div>
-            <div className="col-md-8 col-sm-12 px-5">
-              <h2 className="mb-3 text-end text-light">كل ما تحتاجه قبل الاستثمار فى الذهب</h2>
-              <h3 className="text-end text-light fs-4">
-                وفي نهاية الكتاب، يُقدّم الكاتب نصائح مُختصرة وواضحة للقارئ تُساعده في معرفة أفضل أوقات البيع والاستثمار والتعامل بالذهب. تمّ إعداد هذا الكتاب لمُساعدة القارئ على فهم المعدن الثمين (الذهب)، واتّخاذ قرار الاستثمار به. حيثُ تمّ التطرُّق لأنواع الذهب، وعياراته المُختلفة، وأوزانه، وأسعاره، والعوامل المُؤثّرة عليه؛ ليستنّى له دراسة وفهم وضع السوق قبل اتّخاذ قرار الشراء أو البيع. كما تمّ التنويه للسبائك الذهبية وأنواعها، لنضمن إلمام القارئ بكلّ ما يتعلّق بهذا المعدن الأصفر.
-              </h3>
-            </div>
-          </div>
-        </div>
-        <button onClick={()=>navigate('/book')} className="d-flex m-auto"> احصل على الباكدج مقابل 350دينار كويتى</button>
+          ) : ('')
+        ))}
+        <button onClick={() => navigate('/book')} className="d-flex m-auto"> لقرأه المزيد <FaArrowLeft color="var(--darkblue-color)" size={30} pointsAtX={2}/></button>
       </div>
     </div>
   )
