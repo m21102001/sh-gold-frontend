62eaac2a8351018813ffcea002d15d9c4de5f0c8
m21102001 <mohammedtarek21102001@gmail.com>
2024-02-08 19:07:56 +0200

    updated

--- src/pages/MyWallet/index.jsx
+++ src/pages/MyWallet/index.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import { Footer, Navbar } from "@/layout";
 import axios from "@/api/axios";
 import { LazyLoadImage } from "react-lazy-load-image-component";
 import { AiFillGolden } from "react-icons/ai";
 import { FaMoneyBillAlt } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Tab, TabList, TabPanel, Tabs } from "react-tabs";
 
 const MyWallet = () => {
   const [loading, setLoading] = useState(false)
@@ -49,51 +50,126 @@ const MyWallet = () => {
       });
   }, [])
 
-  // console.log(wallet);
   return (
-    <div style={{ background: 'var(--darkblue-color' }}>
+    <div style={{ background: 'var(--darkblue-color)' }}>
       <Navbar />
       <div className='m-auto d-flex justify-content-center my-5'>
         <span style={{ zIndex: "0", backgroundColor: "#f8d25c", width: "50px", height: "3px", margin: "auto 20px" }}></span>
         <h2 className='text-center comunation fs-1 fw-bold' style={{ color: 'var(--bs-yellow)' }}> محفظتى  </h2>
         <span style={{ zIndex: "0", backgroundColor: "#f8d25c", width: "50px", height: "3px", margin: "auto 20px" }}></span>
       </div>
-      <div className="container text-center pt-5">
-        {!loading && wallet?.map((item, index) => (
-          <div
-            key={index}
-            className="row row-striped shadow-lg p-3 mb-5 bg-body rounded">
-            <div className="col-2 text-right">
-              <LazyLoadImage
-                src={`${import.meta.env.VITE_IMAGE_URL}${item?.image}`}
-                alt={`${item.title}`}
-                style={{
-                  width: '100px',
-                  height: '100px'
-                }}
-              />
-            </div>
-            <div className="col-10 fs-4 text-end" >
-              <h3 className="text-uppercase d-flex flex-row align-items-center justify-content-between">
-                <strong>{item?.title}</strong>
-                <Link
-                  to={`/auth/my-wallet/request/sall`}
-                  state={{ item }}
-                >
-                  <button type="button" className="btn btn-success px-5">
-                    طلب بيع
-                  </button>
-                </Link>
-              </h3>
-              <ul className="list-inline">
-                <li className="list-inline-item mx-3"><AiFillGolden color="gold" size={30} /> {item?.size} جرام</li>
-                <li className="list-inline-item mx-3"> <FaMoneyBillAlt size={30} style={{ margin: '0 10px' }} />{item?.price}</li>
-              </ul>
-              <p>{item?.description}</p>
-            </div>
+      <Tabs>
+        <TabList>
+          <Tab>مقتنياتى من الذهب</Tab>
+          <Tab> كورساتى </Tab>
+          <Tab> كتبي (اونلاين فقط) </Tab>
+        </TabList>
+
+        <TabPanel>
+          <div className="container text-center pt-5">
+            {wallet?.length != 0 ? (
+              !loading && wallet?.map((item, index) => (
+                <div
+                  key={index}
+                  className="row row-striped shadow-lg p-3 mb-5 bg-body rounded">
+                  <div className="col-2 text-right">
+                    <LazyLoadImage
+                      src={`${import.meta.env.VITE_IMAGE_URL}${item?.image}`}
+                      alt={`${item.title}`}
+                      style={{
+                        width: '100px',
+                        height: '100px'
+                      }}
+                    />
+                  </div>
+                  <div className="col-10 fs-4 text-end" >
+                    <h3 className="text-uppercase d-flex flex-row align-items-center justify-content-between">
+                      <strong>{item?.title}</strong>
+                      <Link
+                        to={`/auth/my-wallet/request/sall`}
+                        state={{ item }}
+                      >
+                        <button type="button" className="btn btn-success px-5">
+                          طلب بيع
+                        </button>
+                      </Link>
+                    </h3>
+                    <ul className="list-inline">
+                      <li className="list-inline-item mx-3"><AiFillGolden color="gold" size={30} /> {item?.size} جرام</li>
+                      <li className="list-inline-item mx-3"> <FaMoneyBillAlt size={30} style={{ margin: '0 10px' }} />{item?.price}</li>
+                    </ul>
+                    <p>{item?.description}</p>
+                  </div>
+                </div>
+              ))
+            ) : (<h2 className="text-light">لا يوجد منتجات ذهبية تم شرائها</h2>)}
           </div>
-        ))}
-      </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="container text-center pt-5">
+            {playlist?.playlists?.length != 0 ? (
+              !loading && playlist?.playlists?.map((item, index) => (
+                <div
+                  key={index}
+                  className="row row-striped shadow-lg p-3 mb-5 bg-body rounded">
+                  <div className="col-2 text-right">
+                    <LazyLoadImage
+                      src={`${import.meta.env.VITE_IMAGE_URL}${item?.image}`}
+                      alt={`${item.title}`}
+                      style={{
+                        width: '100px',
+                        height: '100px'
+                      }}
+                    />
+                  </div>
+                  <div className="col-10 fs-4 text-end" >
+                    <h3 className="text-uppercase d-flex flex-row align-items-center justify-content-between">
+                      <strong>{item?.title}</strong>
+                    </h3>
+                    <ul className="list-inline">
+                      <li className="list-inline-item mx-3"> <FaMoneyBillAlt size={30} style={{ margin: '0 10px' }} />{item?.price}</li>
+                    </ul>
+                    <p>{item?.description}</p>
+                  </div>
+                </div>
+              ))
+            ) : (<h2 className="text-light">لا يوجد كورسات تم شرائها</h2>)}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="container text-center pt-5">
+            {book?.books?.length != 0 ? (
+              !loading && book?.books?.map((item, index) => (
+                <div
+                  key={index}
+                  className="row row-striped shadow-lg p-3 mb-5 bg-body rounded">
+                  <div className="col-2 text-right">
+                    <LazyLoadImage
+                      src={`${import.meta.env.VITE_IMAGE_URL}${item?.image}`}
+                      alt={`${item.title}`}
+                      style={{
+                        width: '100px',
+                        height: '100px'
+                      }}
+                    />
+                  </div>
+                  <div className="col-10 fs-4 text-end" >
+                    <h3 className="text-uppercase d-flex flex-row align-items-center justify-content-between">
+                      <strong>{item?.title}</strong>
+                    </h3>
+                    <ul className="list-inline">
+                      <li className="list-inline-item mx-3"> <FaMoneyBillAlt size={30} style={{ margin: '0 10px' }} />{item?.price}</li>
+                    </ul>
+                    <p>{item?.description}</p>
+                  </div>
+                </div>
+              ))
+            ) : (<h2 className="text-light">لا يوجد كتب تم شرائها</h2>)}
+          </div>
+        </TabPanel>
+      </Tabs>
+
+
       <Footer />
     </div>
   )
