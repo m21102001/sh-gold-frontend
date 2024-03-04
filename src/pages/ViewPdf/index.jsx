035bb8de174592db235fa0df643e508503fe5e6e
m21102001 <mohammedtarek21102001@gmail.com>
2024-03-04 16:41:02 +0200

    add new account aws

--- src/pages/ViewPdf/index.jsx
+++ src/pages/ViewPdf/index.jsx
@@ -1,27 +1,19 @@
 import { Footer, Navbar } from "@/layout"
 import { Worker } from '@react-pdf-viewer/core';
 import { Viewer } from '@react-pdf-viewer/core';
-import { useEffect, useState } from "react";
 import { useLocation } from 'react-router-dom';
 
 const ViewPdf = () => {
   const item = useLocation().state?.item;
-  console.log(item)
   // console.log(item)
-  // const [Item, setItem] = useState();
-  // useEffect(() => {
-  //   setItem(item)
-    
-  // }, [item._id])
-  // console.log(Item)
 
-  
   return (
-    <div style={{background:'var(--darkblue-color)'}}>
+    <div style={{ background: 'var(--darkblue-color)' }}>
       <Navbar />
       <Worker workerUrl="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.worker.min.js">
         <Viewer
-          fileUrl={`${import.meta.env.VITE_FILE_URL}${item?.pdf}`}
+          // fileUrl={`${import.meta.env.VITE_FILE_URL}${item?.pdf}`}
+          fileUrl={`https://www.quran-pdf.com/arabic-quran.pdf`}
         />
       </Worker>
       <Footer />
