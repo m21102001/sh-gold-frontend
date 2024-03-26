5344dfa8832a6366ad76ee8636187e7e5a699a6f
mohammed tarek <102931301+m21102001@users.noreply.github.com>
2024-03-26 21:51:15 +0200

    Update index.jsx

--- src/pages/Club/index.jsx
+++ src/pages/Club/index.jsx
@@ -122,7 +122,7 @@ const Club = () => {
                             <div className="card h-100 px-5 shadow-lg card-transmation">
                               <div className="card-body">
                                 <div className="text-center p-3">
-                                  <h5 className="card-title">{item?.title}</h5>
+                                  <h5 className="card-title">{item?.title} <span className="text-danger">وفر 105 دينار</span>span></h5>
                                   <small>Individual</small>
                                   <br /><br />
                                   <span className="h2">{item?.price}</span>
@@ -136,7 +136,7 @@ const Club = () => {
                                       <svg xmlns="http://www.w3.org/2000/svg" width="26" height="26" fill="currentColor" className="bi bi-check" viewBox="0 0 16 16">
                                         <path d="M10.97 4.97a.75.75 0 0 1 1.07 1.05l-3.99 4.99a.75.75 0 0 1-1.08.02L4.324 8.384a.75.75 0 1 1 1.06-1.06l2.094 2.093 3.473-4.425a.267.267 0 0 1 .02-.022z" />
                                       </svg>
-                                      <p className="mb-0 fs-5">{item?.title} <span className="text-danger">وفر 105 دينار</span>span></p>
+                                      <p className="mb-0 fs-5">{item?.title}</p>
                                     </div>
                                   </div>
                                   <hr />
